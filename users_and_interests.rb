c8aa1d2313c70b152f5d5d108190658144700ce9
Tim Cummings <timjc86@gmail.com>
2018-03-27 21:48:53 -0500

    Req2: users names on homepage are links to page for that user

--- users_and_interests.rb
+++ users_and_interests.rb
@@ -5,7 +5,25 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'yaml'
 
-get '/' do
+before do
   @users = YAML.load_file('users.yaml')
+end
+
+get '/' do
   erb :home
+end
+
+get '/user' do
+  redirect '/' if !params[:name] || params[:name].empty?
+
+  @user_index = find_user_index(params[:name])
+  redirect '/' if !@user_index
+
+  erb :user
+end
+
+def find_user_index(name)
+  @users.find_index do |user|
+    user.first.to_s == name.downcase
+  end
 end
