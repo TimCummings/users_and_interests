43574dc996b17fecdc160ec12c23a65e1a97921a
Tim Cummings <timjc86@gmail.com>
2018-03-27 22:19:54 -0500

    Req3: on each user page, display email and interests

--- users_and_interests.rb
+++ users_and_interests.rb
@@ -14,16 +14,9 @@ get '/' do
 end
 
 get '/user' do
-  redirect '/' if !params[:name] || params[:name].empty?
-
-  @user_index = find_user_index(params[:name])
-  redirect '/' if !@user_index
+  @name = params[:name].downcase.to_sym
+  redirect '/' if !@name || @name.empty?
+  redirect '/' unless @users.key? @name
 
   erb :user
-end
-
-def find_user_index(name)
-  @users.find_index do |user|
-    user.first.to_s == name.downcase
-  end
 end
